466accf15af422725147ba5171f14296bdb11fda
GiannfrancoCrovetto <gcrovetto2021@fau.edu>
2026-04-13 17:58:14 -0400

    added more events

--- src/data/events.ts
+++ src/data/events.ts
@@ -111,5 +111,70 @@ export const events: Event[] = [
   },
 
   // 2022-2023 MW2 Season
+  {
+    id: '13',
+    name: 'Major 1',
+    year: 2023,
+    codTitle: 'Modern Warfare 2',
+    winningTeam: 'New York Subliners',
+    acceptedAnswers: ['new york subliners', 'subliners', 'NY', 'NYSL', 'new york', 'ny subliners']
+  },
+  {
+    id: '14',
+    name: 'Major 2',
+    year: 2023,
+    codTitle: 'Modern Warfare 2',
+    winningTeam: 'Atlanta FaZe',
+    acceptedAnswers: ['atlanta faze', 'faze', 'atlanta', 'atl']
+  },
+  {
+    id: '15',
+    name: 'Major 3',
+    year: 2023,
+    codTitle: 'Modern Warfare 2',
+    winningTeam: 'Toronto Ultra',
+    acceptedAnswers: ['toronto ultra', 'ultra', 'tor', 'toronto']
+  },
+  {
+    id: '16',
+    name: 'Major 4',
+    year: 2023,
+    codTitle: 'Modern Warfare 2',
+    winningTeam: 'Los Angeles Thieves',
+    acceptedAnswers: ['los angeles thieves', 'thieves', 'lat', 'la thieves', 'la', 'los angeles']
+  },
+  {
+    id: '17',
+    name: 'Major 5',
+    year: 2023,
+    codTitle: 'Modern Warfare 2',
+    winningTeam: 'New York Subliners',
+    acceptedAnswers: ['new york subliners', 'subliners', 'NY', 'NYSL', 'new york', 'ny subliners']
+  },
+  {
+    id: '18',
+    name: 'Champs',
+    year: 2023,
+    codTitle: 'Modern Warfare 2',
+    winningTeam: 'New York Subliners',
+    acceptedAnswers: ['new york subliners', 'subliners', 'NY', 'NYSL', 'new york', 'ny subliners']
+  },
 
+  // 2025-2026 BO7 Season
+  {
+    id: '19',
+    name: 'Major 1',
+    year: 2026,
+    codTitle: 'Black Ops 7',
+    winningTeam: 'Paris Gentlemates',
+    acceptedAnswers: ['paris gentlemates', 'gentlemates', 'm8', 'paris', 'gm8', 'paris m8']
+  },
+  {
+    id: '20',
+    name: 'Major 2',
+    year: 2026,
+    codTitle: 'Black Ops 7',
+    winningTeam: 'FaZe Vegas',
+    acceptedAnswers: ['vegas faze', 'faze', 'vegas', 'vgs', 'faze vegas']
+  },
 ];
